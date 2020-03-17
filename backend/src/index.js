170f1df9ceae7dee57f418b6e6a528f7a56c8be1
João Denis Rodrigues <joao.denisr@gmail.com>
2020-03-17 17:33:27 -0300

    added postgres

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Client } = require('pg');
 
 const app = express();
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'mysecretpassword',
+    port: 5432,
+})
+client.connect()
 
 app.use(bodyParser.json());
 
