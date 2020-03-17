9de3d5fa0886738cd3e8beda99505f5c10c3f927
João Denis Rodrigues <joao.denisr@gmail.com>
2020-03-17 18:10:17 -0300

    add created new employee action

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -1,7 +1,29 @@
+const db = require("../models/index");
+const Employee = db.employees;
+const Op = db.Sequelize.Op;
 
 module.exports = {
     // Create a new itinerary
-    async create() {
-        
+    async create(request, response) {
+        if (!request.body.name) {
+            response.status(400).send({message: "Name can not be empty!"});
+            return;
+        }
+
+        // Create an Employee
+        const employee = {
+            name: request.body.name
+        };
+
+        // Save Employee in the database
+        Employee.create(employee)
+            .then(data => {
+                response.send(data);
+            })
+            .catch(err => {
+                response.status(500).send({
+                    message: err.message || "Some error occurred while creathe the Employee"
+                });
+            });
     }
 }
