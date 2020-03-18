e06e9b5138c677ca0d9585790f02b6de5c0bb9f9
João Denis Rodrigues <joao.denisr@gmail.com>
2020-03-18 10:07:54 -0300

    added put, get and delete endpoints

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -3,7 +3,7 @@ const Employee = db.employees;
 const Op = db.Sequelize.Op;
 
 module.exports = {
-    // Create a new itinerary
+    // Create a new Employee
     async create(request, response) {
         if (!request.body.name) {
             response.status(400).send({message: "Name can not be empty!"});
@@ -23,6 +23,91 @@ module.exports = {
             .catch(err => {
                 response.status(500).send({
                     message: err.message || "Some error occurred while creathe the Employee"
+                });
+            });
+    },
+
+    // List all employees
+    async findAll(request, response) {
+        Employee.findAll({})
+            .then(data => {
+                response.send(data)
+            })
+            .catch(err => {
+                response.status(500).send({
+                    message: err.message || "Some error ocurred while retrieving employees"
+                });
+            });
+    },
+
+    // Find employee by id
+    async findOne(request, response) {
+        const id = request.params.id;
+
+        Employee.findByPk(id)
+            .then(data => {
+                if (data === null) {
+                    return response.status(404).send({
+                        message: "Employee not found"
+                    });
+                }
+                response.send(data);
+            })
+            .catch(err => {
+                response.status(500).send({
+                    message: "Error retrieving Employee with id " + id
+                });
+            });
+    },
+
+    // Update employee by id
+    async update(request, response) {
+        const id = request.params.id;
+
+        Employee.update(request.body, {
+            where: {id: id}
+        })
+            .then(num => {
+                console.log("Update", num);
+                if (num == 1) {
+                    response.send({
+                        message: "Employee was updated successfully"
+                    });
+                } else {
+                    response.status(404).send({
+                        message: "Cannot update Employee! Not Found!"
+                    });
+                }
+            })
+            .catch(err => {
+                console.log("Update Error", num);
+                response.status(500).send({
+                    message: "Error updating Employee"
+                });
+            });
+    },
+
+    // Delete employee by id
+    async delete(request, response) {
+        const id = request.params.id;
+
+        Employee.destroy({
+            where: {id: id}
+        })
+            .then(num => {
+                if (num == 1) {
+                    response.send({
+                        message: "Employee was deleted successfully!"
+                    });
+                } else {
+                    response.status(404).send({
+                        message: "Cannot delete Employee! Not Found!"
+                    });
+                }
+            })
+            .catch(err => {
+                response.status(500).send({
+                    message: "Error deleting Employee"
                 });
             });
     }
