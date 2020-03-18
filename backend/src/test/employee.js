8294eacdbd2c7402dcef8270e71b94c595f4fc12
João Denis Rodrigues <joao.denisr@gmail.com>
2020-03-18 15:37:49 -0300

    test for get endpoints

--- backend/src/test/employee.js
+++ backend/src/test/employee.js
@@ -55,4 +55,65 @@ describe("Employee", () => {
                 });
         });
     });
+
+    // Test /GET route
+    describe("/GET All Employees", () => {
+        it("it should return all employees", (done) => {
+            let employee = {
+                name: "Employee 1",
+                salary: 2000,
+                role: "Test Role 1"
+            };
+            let employee2 = {
+                name: "Employee 2",
+                salary: 4000,
+                role: "Test Role 2"
+            };
+            Employee.create(employee)
+                .then(data => {})
+                .catch(err => {});
+            Employee.create(employee2)
+                .then(data => {})
+                .catch(err => {});
+
+            chai.request(server)
+                .get("/api/employee")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a("Array");
+                    response.body[0].should.have.property("name");
+                    response.body[0].should.have.property("name").eql("Employee 1");
+                    response.body[0].should.have.property("id");
+                    response.body[0].should.have.property("id").eql(1);
+                    response.body[1].should.have.property("name");
+                    response.body[1].should.have.property("name").eql("Employee 2");
+                    response.body[1].should.have.property("id");
+                    response.body[1].should.have.property("id").eql(2);
+                    done();
+                });
+        });
+    });
+
+    // Test /GET/:id route
+    describe("/GET/:id Employee with id", () => {
+        it("it should return employee with id", (done) => {
+            let employee = {
+                name: "Employee 1",
+                salary: 2000,
+                role: "Test Role 1"
+            };
+
+            chai.request(server)
+                .get("/api/employee/1")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a("Object");
+                    response.body.should.have.property("name");
+                    response.body.should.have.property("name").eql("Employee 1");
+                    response.body.should.have.property("id");
+                    response.body.should.have.property("id").eql(1);
+                    done();
+                });
+        });
+    });
 })
